256293ab28f352593ebb9ffe79cceee11ee6f77f
Nimira43 <n.rabone72@aol.com>
2025-03-07 19:33:20 +0000

    Stripped out Chakra and Vite. Whilst not looking pretty at the moment the functionality of the app seems to be working.

---
+++ frontend/toolchain-2/src/components/Navbar.js
@@ -0,0 +1,36 @@
+
+import {login, logout } from '../features/auth/authSlice'
+import { useSelector, useDispatch } from 'react-redux'
+
+const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+
+  const handleAuthClick = () => {
+    if (isLoggedIn) {
+      dispatch(logout())
+    } else {
+      dispatch(login())
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <h1 size='md' className='logo'>Toolchain</h1>
+          <button
+            className='btn-black'
+            onClick={handleAuthClick}
+          >
+            {isLoggedIn
+              ? 'Logout'
+              : 'Login'
+            }
+          </button>
+        </div>      
+    </div>
+  )
+}
+
+export default Navbar
