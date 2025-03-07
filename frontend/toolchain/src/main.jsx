e71fe88758470a0348a596de3ab9e0427e4a07d5
Nimira43 <n.rabone72@aol.com>
2025-03-07 18:49:19 +0000

    Hitting numerous issues with Chakra again.

--- frontend/toolchain/src/main.jsx
+++ frontend/toolchain/src/main.jsx
@@ -1,14 +1,31 @@
-import { Provider } from '@/components/ui/provider.jsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './app/store.js'
+import { Provider } from 'react-redux';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from './app/store.js';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme as baseTheme } from '@chakra-ui/theme';
+
+const customTheme = {
+  ...baseTheme,
+  colors: {
+    ...baseTheme.colors,
+    primary: {
+      500: '#ff4500',
+    },
+  },
+};
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
+    <ChakraProvider theme={customTheme}>
+      <Provider store={store}>
         <App />
-    </Provider>
-  </StrictMode>,
-)
+      </Provider>
+    </ChakraProvider>
+  </StrictMode>
+);
+
