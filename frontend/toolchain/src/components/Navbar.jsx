72c581fc4c9e376e76e789803f8d6657bc608eca
Nimira43 <n.rabone72@aol.com>
2025-03-07 18:17:17 +0000

    Determining if user is logged in or out and handleAuthClick dispatches actions based on state.

--- frontend/toolchain/src/components/Navbar.jsx
+++ frontend/toolchain/src/components/Navbar.jsx
@@ -1,13 +1,35 @@
 import { Box, Button, Flex, Heading, Spacer } from '@chakra-ui/react'
+import {login, logout } from '../features/auth/authSlice'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn)
+
+  const handleAuthClick = () => {
+    if (isLoggedIn) {
+      dispatch(logout())
+    } else {
+      dispatch(login())
+    }
+  }
+
   return (
     <div>
       <Box bg='#ff4500' px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <Heading size='md' className='logo'>Toolchain</Heading>
           <Spacer />
-          <Button className='btn-black'>Login</Button>
+          <Button
+            className='btn-black'
+            onClick={handleAuthClick}
+          >
+            {isLoggedIn
+              ? 'Logout'
+              : 'Login'
+            }
+          </Button>
         </Flex>
       </Box>      
     </div>
