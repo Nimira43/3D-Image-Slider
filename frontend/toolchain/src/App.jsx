f115fdb1c660dd1ba262508f1ab172ffdd32498c
Nimira43 <n.rabone72@aol.com>
2025-03-07 16:57:02 +0000

    Created basic Navbar component.

--- frontend/toolchain/src/App.jsx
+++ frontend/toolchain/src/App.jsx
@@ -1,12 +1,17 @@
 import { Button, Box, Text, Heading } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <Box p={4}>
-      <Heading mb={4}>Toolchain</Heading>
-      <Text>Experimental Redux Saga / Chakra UI / Blockchain Application</Text>
-      <Button mt={4}>Find Out More</Button>
-    </Box>
+    <>
+      <Navbar />
+      <Box p={4}>
+        <Heading mb={4}>Toolchain</Heading>
+        <Text>Experimental Redux Saga / Chakra UI / Blockchain Application</Text>
+        <Button mt={4}>Find Out More</Button>
+      </Box>
+    </>
+    
   )
 }
 
---
+++ frontend/toolchain/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Box bg='#ff4500' px={4}></Box>      
+    </div>
+  )
+}
+
+export default Navbar
